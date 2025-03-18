42357f2e6214bc914ae62affe9d0d6e87d9c807e
Parinita Chinchalikar <124326752+ParinitaSChinchalikar@users.noreply.github.com>
2025-03-18 14:20:16 -0500

    Time: 1 ms (99.83%) | Memory: 46.7 MB (5.80%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -2,10 +2,9 @@ class Solution
 {
     public int singleNumber(int[] nums) 
     {
-        int n = nums.length;
         int xorr =0;
-        
-        for(int i=0; i<n; i++)
+
+        for(int i=0; i<nums.length; i++)
         {
             xorr = xorr ^ nums[i];
         }
