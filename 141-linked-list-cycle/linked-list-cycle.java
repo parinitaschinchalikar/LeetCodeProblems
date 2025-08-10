1491afd491ad4c39e451cf3bd7d277eecce139bf
Parinita Chinchalikar <124326752+ParinitaSChinchalikar@users.noreply.github.com>
2025-08-10 18:37:04 -0500

    Time: 0 ms (100.00%) | Memory: 44.6 MB (48.85%) - LeetSync

---
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution 
+{
+    public boolean hasCycle(ListNode head) 
+    {
+        //Using HashSet
+        // HashSet<ListNode> set = new HashSet<>();
+
+        // while(head != null)
+        // {
+        //     if(set.contains(head))
+        //     {
+        //         return true;
+        //     }
+        //     set.add(head);
+        //     head = head.next;
+        // }
+        // return false;
+
+        //Using Floyd's Cycle finding algorithm
+        ListNode slow = head, fast = head;
+
+        while(slow != null && fast != null && fast.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(slow == fast)
+                return true;
+        }
+        return false;
+    }
+}
