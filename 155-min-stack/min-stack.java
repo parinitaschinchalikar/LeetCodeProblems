1dddba714ed54f2b8734a159aa812052ec04c04a
Parinita Chinchalikar <124326752+ParinitaSChinchalikar@users.noreply.github.com>
2025-08-10 22:35:15 -0500

    Time: 4 ms (97.73%) | Memory: 44.8 MB (84.84%) - LeetSync

---
+++ 155-min-stack/min-stack.java
@@ -0,0 +1,44 @@
+class MinStack 
+{
+    private Deque<Integer> stack;
+    private Deque<Integer> minStack;
+    
+    public MinStack() 
+    {
+        stack = new ArrayDeque<>();
+        minStack = new ArrayDeque<>();
+    }
+    
+    public void push(int val) 
+    {
+        stack.push(val);
+        if(minStack.isEmpty() || val <= minStack.peek())
+            minStack.push(val);
+    }
+    
+    public void pop() 
+    {
+        int removed = stack.pop();
+        if(removed == minStack.peek())
+            minStack.pop();
+    }
+    
+    public int top() 
+    {
+        return stack.peek();   
+    }
+    
+    public int getMin() 
+    {
+        return minStack.peek();   
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
