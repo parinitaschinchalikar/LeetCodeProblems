316dda8a3ef95c7c81310834e196079ecb0257cc
Parinita Chinchalikar <124326752+parinitaschinchalikar@users.noreply.github.com>
2026-02-09 07:27:22 -0500

    Time: 18 ms (26.95%) | Memory: 50.9 MB (13.21%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -2,16 +2,33 @@ class Solution
 {
     public List<List<String>> groupAnagrams(String[] strs) 
     {
-        Map<String, List<String>> map = new HashMap<>();
+        //Brute Force Algorithm
+        // Map<String, List<String>> map = new HashMap<>();
 
-        for(String s: strs)
+        // for(String s: strs)
+        // {
+        //     char[] c = s.toCharArray();
+        //     Arrays.sort(c);
+        //     String key = new String (c);
+
+        //     map.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
+        // }     
+        // return new ArrayList<>(map.values());
+
+        //Optimal Solution 
+        HashMap<String, List<String>> result = new HashMap<>();
+
+        for(String s : strs)
         {
-            char[] c = s.toCharArray();
-            Arrays.sort(c);
-            String key = new String (c);
-
-            map.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
-        }     
-        return new ArrayList<>(map.values());
+            int[] count = new int[26];
+            for(char c : s.toCharArray())
+            {
+                count[c - 'a']++;
+            }
+            String key = Arrays.toString(count);
+            result.putIfAbsent(key, new ArrayList<>());
+            result.get(key).add(s);
+        }
+        return new ArrayList<>(result.values());
     }
 }
