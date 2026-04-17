b359a6eee5d0816005e7fece6cd02137bc3b3334
Parinita Chinchalikar <124326752+parinitaschinchalikar@users.noreply.github.com>
2026-04-17 08:28:11 -0400

    Time: 15 ms (64.79%) | Memory: 93.4 MB (50.83%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) 
+    {
+        //Brute Force
+        /*
+        for(int i=0; i< nums.length; i++)
+        {
+            for(int j=i+1; j< nums.length; j++)
+            {
+                if(nums[i] == nums[j])
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+        */
+
+        //Optimal Solution
+        Set<Integer> set = new HashSet<>();
+        for(int num : nums)
+        {
+            if(set .contains(num))
+                return true;
+            
+            set.add(num);
+        }
+        return false;
+    }
+}
