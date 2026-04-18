8daf72a921799625ec7b329b3f29955f68c7635b
Parinita Chinchalikar <124326752+parinitaschinchalikar@users.noreply.github.com>
2026-04-18 19:39:09 -0400

    Time: 1 ms (99.92%) | Memory: 94.5 MB (36.35%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -2,30 +2,23 @@ class Solution {
     public int maxProfit(int[] prices) 
     {
         //Greedy One pass solution
-        // int buy = Integer.MAX_VALUE;
-        // int profit = 0;
-
-        // for(int i=0; i< prices.length; i++)
-        // {
-        //     if(prices[i] < buy)
-        //     {
-        //         buy = prices[i];
-        //     }
-        //     else if(prices[i]-buy > profit)
-        //     {
-        //         profit = prices[i] - buy;
-        //     }
-        // }
-
-        //Dynamic Programming
         int buy = Integer.MAX_VALUE;
         int profit = 0;
 
-        for (int price : prices) 
+        for(int i=0; i< prices.length; i++)
         {
-            buy = Math.min(buy, price);
-            profit = Math.max(profit, price - buy);
+            if(prices[i] < buy)
+            {
+                buy = prices[i];
+            }
+            else if(prices[i]-buy > profit)
+            {
+                profit = prices[i] - buy;
+            }
         }
         return profit;
     }
 }
+
+//Time complexity : O(n)
+//Space complexity : O(1)
