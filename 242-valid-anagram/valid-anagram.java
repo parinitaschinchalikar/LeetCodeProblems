fe84b341ad500c1d8ab132c39dbcee7f073587cf
Parinita Chinchalikar <124326752+parinitaschinchalikar@users.noreply.github.com>
2026-04-19 19:12:32 -0400

    Time: 5 ms (61.73%) | Memory: 43.9 MB (97.85%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,26 +1,39 @@
 class Solution {
     public boolean isAnagram(String s, String t) 
     {
-        //Optimal solution
+        //Brute Force - Sorting
+        // if(s.length() != t.length())
+        // {
+        //     return false;
+        // }
+        // char[] str1 = s.toCharArray();
+        // char[] str2 = t.toCharArray();
+        // Arrays.sort(str1);
+        // Arrays.sort(str2);
+        // return Arrays.equals(str1, str2);
 
-        if(s.length() != t.length())
+        //Time complexity: O(nlogn) 
+        //Space complexity:O(1)
+
+        //Optimal 
+        if(s.length() !=t.length())
         {
             return false;
         }
         int[] count = new int[26];
-
-        for(int i =0; i < s.length(); i++)
+        for(int i=0; i< s.length(); i++)
         {
             count[s.charAt(i) - 'a']++;
             count[t.charAt(i) - 'a']--;
         }
-
         for(int value : count)
         {
             if(value != 0)
                 return false;
         }
-
         return true;
     }
 }
+
+//Time complexity : O(n)
+//Space complexity : O(1)
