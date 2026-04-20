4a0cc482343af98f11e094083bb467f650e95856
Parinita Chinchalikar <124326752+parinitaschinchalikar@users.noreply.github.com>
2026-04-20 18:32:53 -0400

    Time: 2 ms (95.15%) | Memory: 72.1 MB (30.86%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -2,21 +2,47 @@ class Solution
 {
     public int[] productExceptSelf(int[] nums) 
     {
+        //Brute Force algorithm
+        /**int n = nums.length;
+        int [] answer = new int [n];
+
+        for(int i=0; i<n; i++)
+        {
+            int product = 1;
+
+            for(int j=0; j<n; j++)
+            {
+                if(i!=j)
+                {
+                    product *= nums[j];
+                }
+            }
+            answer[i] = product;
+        }
+        return answer;
+        */
+
+//Time complexity : O(n^2)
+//Space complexity : O(n)
+
+        //Optimal Solution - Prefix + Suffix
         int n = nums.length;
-        int[] output = new int[n];
-        output[0] = 1;
+        int[] prefix = new int[n];
+        prefix[0] = 1;
 
         for(int i=1; i<n; i++)
         {
-            output[i] = output[i-1] * nums[i-1];
+            prefix[i] = prefix[i-1] * nums[i-1];
         }
 
         int suffixProduct =1;
         for(int i=n-1; i>=0; i--)
         {
-            output[i] = output[i] * suffixProduct;
+            prefix[i] = prefix[i] * suffixProduct;
             suffixProduct *= nums[i];
         }
-        return output;
+        return prefix;
     }
 }
+//Time complexity : O(n)
+//Space complexity : O(n)
