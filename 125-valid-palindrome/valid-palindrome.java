d6679ff3fc267abd15eacc579030e504585c7ad2
Parinita Chinchalikar <124326752+parinitaschinchalikar@users.noreply.github.com>
2026-04-21 10:58:40 -0400

    Time: 2 ms (99.39%) | Memory: 44.3 MB (67.08%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,23 +1,47 @@
 class Solution 
 {
-    public boolean isPalindrome(String s) 
+    //Brute Force Algorithm
+    // public boolean isPalindrome(String s) 
+    // {
+    //     String str = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+    //     return isPalindromeHelper( str, 0);
+    // }
+
+    // private boolean isPalindromeHelper(String str, int i) 
+    // {
+    //     if (i >= str.length() / 2) 
+    //     {
+    //         return true;
+    //     }
+
+    //     if (str.charAt(i) != str.charAt(str.length() - i - 1)) 
+    //     {
+    //         return false;
+    //     }
+
+    //     return isPalindromeHelper(str, i + 1);
+    // }
+
+    //Optimal Solution 
+    public boolean isPalindrome(String s)
     {
-        String str = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
-        return isPalindromeHelper( str, 0);
-    }
+        int left=0, right=s.length()-1;
 
-    private boolean isPalindromeHelper(String str, int i) 
-    {
-        if (i >= str.length() / 2) 
+        while(left < right)
         {
-            return true;
-        }
+            while(left < right && !Character.isLetterOrDigit(s.charAt(left)))
+                left++;
+            
+            while(left < right && !Character.isLetterOrDigit(s.charAt(right)))
+                right--;
 
-        if (str.charAt(i) != str.charAt(str.length() - i - 1)) 
-        {
-            return false;
+            if(Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right)))
+                return false;
+        left++;
+        right--;
         }
-
-        return isPalindromeHelper(str, i + 1);
+        return true;
     }
 }
+//Time complexity : O(n)
+//Space complexity : O(n)
