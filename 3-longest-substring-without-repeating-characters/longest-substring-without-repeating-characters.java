dea4cf4680e8464738fde43642850ae2d11c9451
Parinita Chinchalikar <124326752+parinitaschinchalikar@users.noreply.github.com>
2026-04-25 07:59:32 -0400

    Time: 6 ms (69.51%) | Memory: 46.5 MB (39.53%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,11 +1,41 @@
-class Solution 
+/**class Solution 
 {
     public int lengthOfLongestSubstring(String s) 
     {
-        HashSet<Character> set = new HashSet<>();
-        int l=0,res=0;
+        //Brute Force
+        int maxLen =0;
 
-        for(int r=0;r<s.length();r++)
+        for(int i=0; i<s.length(); i++)
+        {
+            Set<Character> set = new HashSet<>();
+
+            for(int j=i; j<s.length(); j++)
+            {
+                char ch = s.charAt(j);
+
+                if(set.contains(ch))
+                    break;
+                
+                set.add(ch);
+                maxLen = Math.max(maxLen, j-i+1);
+            }
+        }
+        return maxLen;
+    }
+}
+Time complexity : O(n^2)
+Space complexity : O(n)
+ */
+
+class Solution 
+{
+    public int lengthOfLongestSubstring(String s)
+    {
+        //Optimal Solution 
+        HashSet<Character> set = new HashSet<>();
+        int l=0, result =0;
+
+        for(int r=0; r<s.length(); r++)
         {
             while(set.contains(s.charAt(r)))
             {
@@ -13,8 +43,13 @@ class Solution
                 l++;
             }
             set.add(s.charAt(r));
-            res = Math.max(res, r-l+1);
+            result = Math.max(result, r-l+1);
         }
-        return res;
+        return result;
     }
 }
+
+/**
+time complexity :O(n)
+space complexity : O(min(n, charset))
+*/
