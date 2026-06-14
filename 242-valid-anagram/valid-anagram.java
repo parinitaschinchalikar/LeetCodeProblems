08aee7d5238f2cd1ee7c11408ded67e414fa2ef0
Parinita Chinchalikar <124326752+parinitaschinchalikar@users.noreply.github.com>
2026-06-14 12:21:36 -0400

    Time: 5 ms (62.19%) | Memory: 44.3 MB (82.00%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,33 +1,28 @@
 class Solution {
-    public boolean isAnagram(String s, String t) 
-    {
-        //Brute Force - Sorting
-        // if(s.length() != t.length())
-        // {
-        //     return false;
-        // }
-        // char[] str1 = s.toCharArray();
-        // char[] str2 = t.toCharArray();
-        // Arrays.sort(str1);
-        // Arrays.sort(str2);
-        // return Arrays.equals(str1, str2);
-
-        //Time complexity: O(nlogn) 
-        //Space complexity:O(1)
-
+    public boolean isAnagram(String s, String t){
+        /**Brute Force - Sorting
+        if(s.length() != t.length()){
+            return false;
+        }
+        char[] str1 = s.toCharArray();
+        char[] str2 = t.toCharArray();
+        Arrays.sort(str1);
+        Arrays.sort(str2);
+        return Arrays.equals(str1, str2);
+/**
+Time complexity: O(nlogn) 
+Space complexity:O(1)
+*/
         //Optimal 
-        if(s.length() !=t.length())
-        {
+        if(s.length() !=t.length()){
             return false;
         }
         int[] count = new int[26];
-        for(int i=0; i< s.length(); i++)
-        {
+        for(int i=0; i< s.length(); i++){
             count[s.charAt(i) - 'a']++;
             count[t.charAt(i) - 'a']--;
         }
-        for(int value : count)
-        {
+        for(int value : count){
             if(value != 0)
                 return false;
         }
@@ -35,5 +30,7 @@ class Solution {
     }
 }
 
-//Time complexity : O(n)
-//Space complexity : O(1)
+/**
+Time complexity : O(n)
+Space complexity : O(1)
+*/
