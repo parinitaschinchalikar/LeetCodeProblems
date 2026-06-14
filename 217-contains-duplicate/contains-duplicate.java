480f82c4e7c2aa74f219beb0e469208725133ab6
Parinita Chinchalikar <124326752+parinitaschinchalikar@users.noreply.github.com>
2026-06-13 22:39:11 -0400

    Time: 45 ms (5.08%) | Memory: 93.1 MB (70.73%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -20,7 +20,7 @@ class Solution {
         Set<Integer> set = new HashSet<>();
         for(int num : nums)
         {
-            if(set .contains(num))
+            if(set.contains(num))
                 return true;
             
             set.add(num);
