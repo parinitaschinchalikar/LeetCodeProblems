86dae3a2780dd3f072b7ded7942a9d247990d9f7
Parinita Chinchalikar <124326752+parinitaschinchalikar@users.noreply.github.com>
2026-06-26 13:57:26 -0400

    Time: 2 ms (99.38%) | Memory: 46.9 MB (83.23%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -2,33 +2,35 @@ class Solution
 {
     public int[] twoSum(int[] nums, int target) 
     {
-        //Brute force 
-        // for(int i=0; i<nums.length; i++){
-        //     for(int j=i+1; j<nums.length; j++){
-        //         if(nums[i]+nums[j]==target){
-        //             return new int[]{i,j};
+        // Optimal Solution
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++)
+        {
+            int compliment = target - nums[i];
+            if(map.containsKey(compliment))
+            {
+                return new int[] {map.get(compliment), i};
+            }
+
+            map.put(nums[i], i);
+        }
+
+        return new int[] {};
+
+
+        // // Brute force
+        // for(int i = 0; i < nums.length; i++)
+        // {
+        //     for(int j = i + 1; j < nums.length; j++)
+        //     {
+        //         if(nums[i] + nums[j] == target)
+        //         {
+        //             return new int[] {i, j};
         //         }
         //     }
         // }
-        // return new int[]{};
 
-        //Optimal Solution
-        Map<Integer, Integer> map = new HashMap<>();
-
-        for(int i=0; i<nums.length; i++)
-        {
-            int cur = nums[i];
-            // cur + x = target
-            //x = target - cur
-            int x = target -cur;
-            if(map.containsKey(x))
-            {
-                return new int[] {map.get(x), i};
-            }
-            map.put(cur, i);
-        }
-        return null;
+        // return new int[] {};
     }
 }
-//Time : O(n)
-//Space : O(n)
