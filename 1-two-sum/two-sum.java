3b02f9a26e6bd357dc6816b2a45fbee49fb6f095
Parinita Chinchalikar <124326752+parinitaschinchalikar@users.noreply.github.com>
2026-07-03 12:22:07 -0400

    Time: 2 ms (99.36%) | Memory: 47.2 MB (23.01%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,36 +1,33 @@
-class Solution 
-{
-    public int[] twoSum(int[] nums, int target) 
-    {
-        // Optimal Solution
+//Brute Force
+// class Solution{
+//     public int[] twoSum(int[]nums, int target){
+//         for(int i=0; i< nums.length; i++){
+//             for(int j=i+1; j<nums.length; j++){
+//                 if(nums[i] + nums[j] == target)
+//                     return new int[]{i, j};
+//             }
+//         }
+//         return new int[]{0};
+//     }
+// }
+//Time : O(n^2)
+//Space : O(1)
+
+//Optimal Solution - Using HashMap
+class Solution{
+    public int[] twoSum(int[] nums, int target){
         Map<Integer, Integer> map = new HashMap<>();
 
-        for(int i = 0; i < nums.length; i++)
-        {
-            int compliment = target - nums[i];
-            if(map.containsKey(compliment))
-            {
-                return new int[] {map.get(compliment), i};
-            }
-
+        // loop to check if complement exists 
+        for(int i=0; i<nums.length; i++){
+            int complement = target - nums[i];
+            if(map.containsKey(complement))
+                return new int[]{map.get(complement), i};
+            
             map.put(nums[i], i);
-        }
-
+        } 
         return new int[] {};
-
-
-        // // Brute force
-        // for(int i = 0; i < nums.length; i++)
-        // {
-        //     for(int j = i + 1; j < nums.length; j++)
-        //     {
-        //         if(nums[i] + nums[j] == target)
-        //         {
-        //             return new int[] {i, j};
-        //         }
-        //     }
-        // }
-
-        // return new int[] {};
     }
 }
+//Time : O(n)
+//Space : O(n)
