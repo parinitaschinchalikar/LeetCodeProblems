d311a8afe1380c6a1952d8e1c9b1c2688531992c
Parinita Chinchalikar <124326752+parinitaschinchalikar@users.noreply.github.com>
2026-07-03 15:12:23 -0400

    Time: 18 ms (37.78%) | Memory: 108 MB (5.75%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -1,34 +1,62 @@
-class Solution {
-    public boolean containsDuplicate(int[] nums) 
-    {
-        //Brute Force
-        /*
-        for(int i=0; i< nums.length; i++)
-        {
-            for(int j=i+1; j< nums.length; j++)
-            {
-                if(nums[i] == nums[j])
-                {
-                    return true;
-                }
-            }
-        }
-        return false;
-        */
+// class Solution {
+//     public boolean containsDuplicate(int[] nums) 
+//     {
+//         // Optimal solution
+//         HashSet<Integer> set = new HashSet<>();
 
-        //Optimal Solution
-        Set<Integer> set = new HashSet<>();
-        for(int num : nums)
-        {
+//         for(int i = 0; i < nums.length; i++)
+//         {
+//             if(set.contains(nums[i]))
+//             {
+//                 return true;
+//             }
+//             set.add(nums[i]);
+//         }
+
+//         return false;
+
+
+//         // // Brute Force
+//         // for(int i = 0; i < nums.length; i++)
+//         // {
+//         //     for(int j = i + 1; j < nums.length; j++)
+//         //     {
+//         //         if(nums[i] == nums[j])
+//         //         {
+//         //             return true;
+//         //         }
+//         //     }
+//         // } 
+
+//         // return false;
+//     }
+// }
+
+// class Solution{
+//     public boolean containsDuplicate(int[] nums){
+//         for(int i=0; i< nums.length; i++){
+//             for(int j=i+1; j<nums.length; j++){
+//                 if(nums[i] == nums[j])
+//                     return true;
+//             }
+//         }
+//         return false;
+//     }
+// }
+//Time : O(n^2)
+//Space : O(1)
+
+class Solution{
+    public boolean containsDuplicate(int[] nums){
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int num : nums){
             if(set.contains(num))
                 return true;
-            
             set.add(num);
         }
         return false;
     }
 }
-/**
-Time complexity : O(n)
-Space complexity : O(n)
- */
+//Time : O(N)
+//Space : O(N)
