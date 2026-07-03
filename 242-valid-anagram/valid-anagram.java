f0a4908049daf4e99590efaf101c7d09894db6fb
Parinita Chinchalikar <124326752+parinitaschinchalikar@users.noreply.github.com>
2026-07-03 18:07:27 -0400

    Time: 5 ms (61.63%) | Memory: 44.1 MB (89.46%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,35 +1,65 @@
-class Solution {
+// class Solution {
+//     public boolean isAnagram(String s, String t) 
+//     {
+//         // Optimal
+//         if(s.length() != t.length())
+//         {
+//             return false;
+//         } 
+
+//         int[] count = new int[26];
+
+//         for(int i = 0; i < s.length(); i++)
+//         {
+//             count[s.charAt(i) - 'a']++;
+//             count[t.charAt(i) - 'a']--;
+//         }
+
+//         for(int c : count)
+//         {
+//             if(c != 0)
+//             {
+//                 return false;
+//             }
+//         }
+
+//         return true;
+
+
+//         // // Brute Force
+//         // if(s.length() != t.length())
+//         // {
+//         //     return false;
+//         // }
+
+//         // char[] s1 = s.toCharArray();
+//         // char[] t1 = t.toCharArray();
+
+//         // Arrays.sort(s1);
+//         // Arrays.sort(t1);
+
+//         // return Arrays.equals(s1, t1);
+//     }
+// }
+
+class Solution{
     public boolean isAnagram(String s, String t){
-        /**Brute Force - Sorting
-        if(s.length() != t.length()){
+        if(s.length() != t.length())
             return false;
-        }
-        char[] str1 = s.toCharArray();
-        char[] str2 = t.toCharArray();
-        Arrays.sort(str1);
-        Arrays.sort(str2);
-        return Arrays.equals(str1, str2);
-/**
-Time complexity: O(nlogn) 
-Space complexity:O(1)
-*/
-        //Optimal 
-        if(s.length() !=t.length()){
-            return false;
-        }
+
         int[] count = new int[26];
-        for(int i=0; i< s.length(); i++){
-            count[s.charAt(i) - 'a']++;
-            count[t.charAt(i) - 'a']--;
+
+        for(int i = 0; i<s.length(); i++){
+            count[s.charAt(i)- 'a']++;
+            count[t.charAt(i)- 'a']--;
         }
-        for(int value : count){
-            if(value != 0)
+
+        for(int c : count){
+            if(c!=0)
                 return false;
         }
         return true;
     }
 }
-/**
-Time complexity : O(n)
-Space complexity : O(1)
-*/
+//Time : O(N)
+//Complexity : O(1)
