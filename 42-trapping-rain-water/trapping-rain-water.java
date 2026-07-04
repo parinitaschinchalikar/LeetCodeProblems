72a7c7b38fd370d9f5285c38a14f71fe1c6e9e7c
Parinita Chinchalikar <124326752+parinitaschinchalikar@users.noreply.github.com>
2026-07-04 16:52:33 -0400

    Time: 0 ms (100.00%) | Memory: 47.2 MB (97.60%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,41 +1,6 @@
-// class Solution
-// {
-//     public int trap(int[] height) 
-//     {
-//         //Brute Force 
-//         if(height == null || height.length == 0)
-//         {
-//             return 0;
-//         }
-        
-//         int n = height.length;
-//         int water =0;
-
-//         for(int i=0; i<n; i++)
-//         {
-//             int leftMax = height[i];
-//             int rightMax = height[i];
-
-//             for(int l=0; l<i; l++)
-//             {
-//                 leftMax = Math.max(leftMax, height[l]);
-//             }
-//             for(int r=i; r<n; r++)
-//             {
-//                 rightMax = Math.max(rightMax, height[r]);
-//             }
-//             water += Math.min(leftMax, rightMax) - height[i];
-//         }
-//         return water;
-//     }
-// }
-//Time complexity : O(n)
-//Space complexity : O(1)
-
-
 class Solution 
 {
-    public int trap(int[] height)
+    public int trap(int[] height) 
     {
         //Optimal Solution
         if( height == null || height.length == 0 )
@@ -61,8 +26,10 @@ class Solution
                 res += rightMax - height[r];
             }
         }
-        return res;
+        return res;    
     }
 }
-//Time complexity : O(n)
-//Space complexity : O(1)
+/**
+Time : O(N)
+Space : O(1)
+ */
